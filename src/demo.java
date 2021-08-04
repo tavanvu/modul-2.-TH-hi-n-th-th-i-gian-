8d162638bb21f44c2019da10164241dcd92e4db9
tavanvu <vanvu1998phucduc@gmail.com>
2021-08-04 11:08:47 +0700

    file

---
+++ src/demo.java
@@ -0,0 +1,16 @@
+public class demo {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d =20.5d;
+        boolean b =true;
+        char c = 'a';
+        String s = "hà nội";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
